2f2016c283a30eef58120f81ad46a54539ddf1b8
Aaron Bukowski <abukowski21@gmail.com>
2026-06-08 14:48:30 -0400

    put in a newline

--- tiegcmrun.py
+++ tiegcmrun.py
@@ -9,7 +9,8 @@ import sys
 
 # Add the tiegcmrun package directory to the module search path so that
 # the internal imports (compile, misc, config, etc.) resolve correctly.
-sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "tiegcmrun"))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                "tiegcmrun"))
 
 from tiegcmrun import tiegcmrun
 
