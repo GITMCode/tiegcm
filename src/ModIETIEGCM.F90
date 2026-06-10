604e9ced3ce902ad745b17f004918d59492bacaa
Aaron Bukowski <abukowski21@gmail.com>
2026-06-10 17:05:28 -0400

    Port over MILE aurora, validation has passed.
    - machine-precision agreement with pre-electrodynamics state
    - compiles with & without electrodynamics
    - no errors, nan's etc.
    - only change from default is that aurora model name was added to outputs (emery)

--- src/ModIETIEGCM.F90
+++ src/ModIETIEGCM.F90
@@ -1,59 +1,66 @@
 module ModIETIEGCM
 #ifdef HAVEMILE
-  use ModIE, only: ieModel, iHeelis_, iWeimer05_
+  use ModIE
+  use params_module, only: nmlon, nmlat
   implicit none
-  
+
   type(ieModel), save :: ie
 
 contains
 
   subroutine init_ie()
+    use input_module, only: potential_model, aurora_model
     implicit none
-    ! Basic initialization if needed
-    ie%iDebugLevel = 1
+
+    ie = iemodel()
+
+    ! Set the names of the models (from .inp namelist)
+    call ie%efield_model(potential_model)
+    if (aurora_model /= 'emery') then
+      call ie%aurora_model(aurora_model)
+    else
+      call ie%aurora_model('zero')
+    endif
+
+    ! Point to Electrodynamics data
+    call ie%model_dir("../ext/Electrodynamics/data/ext/")
+    
+    ! Initialize the IE library
+    call ie%init()
+
   end subroutine init_ie
 
-  subroutine update_ie_potential()
+  subroutine update_ie_potential(byimf_in, bzimf_in, swvel_in, swden_in)
     use params_module, only: nmlon, nmlonp1, nmlat
     use cons_module, only: ylonm, ylatm, pi
-    use input_module, only: potential_model
+    use input_module, only: potential_model, ctpoten, power
     use magfield_module, only: sunlons
     use pdynamo_module, only: phihm, nmlat0
-    use ModTiegcmHeelis, only: offc_ie=>offc, dskofc_ie=>dskofc, &
-                               phin_ie=>phin, phid_ie=>phid, theta0_ie=>theta0, &
-                               psie_ie=>psie, psim_ie=>psim, pcen_ie=>pcen, &
-                               phidm0_ie=>phidm0, phidp0_ie=>phidp0, &
-                               phinm0_ie=>phinm0, phinp0_ie=>phinp0, rr1_ie=>rr1
-    use aurora_module, only: offc, dskofc, phin, phid, theta0, &
-                             psie, psim, pcen, phidm0, phidp0, &
-                             phinm0, phinp0, rr1
-    use ModIE, only: iHeelis_, iWeimer05_
+    use init_module, only: iyear, iday, uthr
+    use wei05sc, only: cvt2md
+    use aurora_module, only: ie_eflux_mag, ie_avee_mag
+
     implicit none
 
     integer :: iMlt, iLat
-    real, allocatable :: potential(:,:)
+    real, allocatable :: potential(:, :)
+    real, intent(in), optional :: byimf_in, bzimf_in, swvel_in, swden_in
+    integer :: imo, ida, ihour, imin, isec
 
-    integer, external :: efield_interpret_name
 
-    ! Map TIEGCM string to IE model ID using Electrodynamics native interpreter
-    ie%iEfield_ = efield_interpret_name(potential_model)
-
-    if (ie%iEfield_ == iHeelis_) then
-      ! Copy parameters from aurora_module
-      offc_ie = offc
-      dskofc_ie = dskofc
-      phin_ie = phin
-      phid_ie = phid
-      theta0_ie = theta0
-      psie_ie = psie
-      psim_ie = psim
-      pcen_ie = pcen
-      phidm0_ie = phidm0
-      phidp0_ie = phidp0
-      phinm0_ie = phinm0
-      phinp0_ie = phinp0
-      rr1_ie = rr1
+    if (ie%iEfield_ == iWeimer05_) then
+      if (present(byimf_in)) ie%needImfBy = byimf_in
+      if (present(bzimf_in)) ie%needImfBz = bzimf_in
+      if (present(swvel_in)) ie%needSwV = swvel_in
+      if (present(swden_in)) ie%needSwN = swden_in
     endif
+
+      ! Update time in Electrodynamics (this computes tilt for Weimer05!)
+      call cvt2md(6, iyear, iday, imo, ida)
+      ihour = int(uthr)
+      imin = int((uthr - real(ihour))*60.0)
+      isec = 0
+      call ie%time_ymdhms(iyear, imo, ida, ihour, imin, isec)
 
     ! Update grid dynamically because MLT changes with time 'sunlons'
     if (ie%neednMLTs /= nmlon .or. ie%neednLats /= nmlat) then
@@ -67,16 +74,19 @@ contains
 
     do iLat = 1, nmlat
       do iMlt = 1, nmlon
-        ie%needLats(iMlt, iLat) = ylatm(iLat) * (180.0 / pi)
-        ie%needMlts(iMlt, iLat) = ((ylonm(iMlt) - sunlons(1)) * 12.0 / pi) + 12.0
+        ie%needLats(iMlt, iLat) = ylatm(iLat)*(180.0/pi)
+        ie%needMlts(iMlt, iLat) = ((ylonm(iMlt) - sunlons(1))*12.0/pi) + 12.0
       enddo
     enddo
 
+    ie%needHpN = power
+    ie%needHpS = power
+
     if (ie%iEfield_ > 0) then
       allocate(potential(nmlon, nmlat))
+
       call ie%get_potential(potential)
-      
-      ! Map back to TIEGCM phihm (kV -> V is handled inside, potential is V)
+
       do iLat = 1, nmlat
         do iMlt = 1, nmlon
           phihm(iMlt, iLat) = potential(iMlt, iLat)
@@ -84,7 +94,9 @@ contains
         ! Periodic point
         phihm(nmlonp1, iLat) = phihm(1, iLat)
       enddo
+
       deallocate(potential)
+      call ie%get_electron_diffuse_aurora(ie_eflux_mag, ie_avee_mag)
     else
       ! NONE potential
       do iLat = 1, nmlat0
